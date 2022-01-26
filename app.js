b356aa7ea4e0cbff793036173ba7d3672b3f534d
Alba233 <alba@alba.fr>
2022-01-26 16:36:59 +0100

    css responsive menu burger

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const burger = document.querySelector('.burger')
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('nav-list');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () =>{
+        burger.classList.remove("active");
+        navMenu.classList.remove("active");
+
+    }))
